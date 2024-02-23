5a8ccc8ca14d34cb06915b18d07ca19acfeb6ef6
jacnud <jacopo.nudo@transportenvironment.org>
2024-02-23 12:49:53 +0100

    line to skip do exception in case of wrong path.

--- T&A/python/emissions_counter.py
+++ T&A/python/emissions_counter.py
@@ -19,10 +19,10 @@ ports_geometry = [Point(xy) for xy in zip(ports_df['LONGITUDE'], ports_df['LATIT
 ports_gdf = gpd.GeoDataFrame(ports_df, geometry=ports_geometry, crs="EPSG:4326")
 
 # Aggiungi un buffer ai porti
-ports_gdf['geometry'] = ports_gdf['geometry'].buffer(0.12)  # Impostiamo 0.001 come distanza del buffer (da adattare)
+ports_gdf['geometry'] = ports_gdf['geometry'].buffer(0.8)  # Impostiamo 0.001 come distanza del buffer (da adattare)
 
 # Read the file into a DataFrame
-columns_needed = ['DIST_SGNL', 'NAVIGATIONAL_STATUS', 'LONGITUDE', 'LATITUDE', 'E_CO2_kg']
+columns_needed = ['DIST_SGNL', 'PORT_STOP', 'LONGITUDE', 'LATITUDE', 'E_CO2_kg']
 files = [filename for filename in os.listdir(folder_path) if "hourly" in filename and filename.endswith(".csv")]
 
 
@@ -33,8 +33,7 @@ for filename in tqdm(files):
     
         # Read only the specified columns from the CSV file
         signals_df = pd.read_csv(file_path, usecols=columns_needed, low_memory=False)
-        signals_df = pd.read_csv(file_path, low_memory=False)
-        signals_df = signals_df[(signals_df['DIST_SGNL'] < 3) | (signals_df['NAVIGATIONAL_STATUS'].isin(['at_berth', 'anchored']))]
+        signals_df = signals_df[(signals_df['DIST_SGNL'] < 3) & ~signals_df['PORT_STOP'].isna()]
         if len(signals_df)==0:
             continue
         
@@ -45,12 +44,12 @@ for filename in tqdm(files):
         # Esegui l'operazione di join spaziale
         joined = gpd.sjoin(signals_gdf, ports_gdf, how="inner", op="intersects")
         joined['Distance_to_Port'] = joined.apply( lambda row: haversine_distance( row['LONGITUDE_left'],row['LATITUDE_left'], row['LONGITUDE_right'],row['LATITUDE_right']),axis=1)
-        joined=joined[joined['Distance_to_Port']<8]
-        joined=joined[['E_CO2_kg','PORT_NAME_right']]
-        aggregated_data = joined.groupby('PORT_NAME_right')['E_CO2_kg'].sum().reset_index()
+        joined=joined[joined['Distance_to_Port']<6]
+        joined=joined[['E_CO2_kg','PORT_NAME']]
+        aggregated_data = joined.groupby('PORT_NAME')['E_CO2_kg'].sum().reset_index()
         
         for index, row in aggregated_data.iterrows():
-            port_name = row['PORT_NAME_right']
+            port_name = row['PORT_NAME']
             co2_value = row['E_CO2_kg']
             
             # Check if the port_name already exists in the dictionary
@@ -62,5 +61,6 @@ for filename in tqdm(files):
                 port_co2_dict[port_name] = co2_value
         
 
+pd.DataFrame(list(port_co2_dict.items()), columns=['PORT_NAME', 'E_CO2_kg']).to_csv('C:/Users/TE/Documents/UK/T&A/stored_data/emissions_in port_6nm')
 
 
